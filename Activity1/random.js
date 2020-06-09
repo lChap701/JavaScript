98b35f23dd06a75fa59794d3b9586e2c81cb2d9c
lChap701 <57417221+lChap701@users.noreply.github.com>
2020-06-09 10:12:03 -0500

    Lab Activity 1

---
+++ Activity1/random.js
@@ -0,0 +1,16 @@
+/*
+   New Perspectives on JavaScript
+   Tutorial 1
+   Review Assignment
+
+
+   Function List:
+   randomInteger()
+      Used to reverse the order of characters in a text string
+
+*/
+
+
+function randomInteger(size) {
+   return Math.floor(size*Math.random());
+}
